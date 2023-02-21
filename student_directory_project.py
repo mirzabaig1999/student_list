21eebf8c5fc9cc74058f930cb510f465c666a087
mahmood baig <baig4077@gmail.com>
2023-02-21 07:59:25 +0530

    commit 1

---
+++ student_directory_project.py
@@ -0,0 +1,134 @@
+
+import csv
+student_fields = ['roll_no', 'name', 'age', 'sex']
+student_database = 'students.csv'
+
+
+def display_menu():
+    print(" Welcome to Student Management System")
+    print(" ")
+    print("1. Add New Student")
+    print("2. View Students")
+    print("3. Update Student")
+    print("4. Delete Student")
+    print("5. Quit")
+
+
+def add_student():
+    print("Add Student Information")
+    print(" ")
+    global student_fields
+    global student_database
+
+    student_data = []
+    for field in student_fields:
+        value = input("Enter " + field + ": ")
+        student_data.append(value)
+
+    with open(student_database, "a", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerows([student_data])
+
+    print("Data saved successfully")
+    input("Press any key to continue")
+    return
+
+
+def view_students():
+    global student_fields
+    global student_database
+
+    print("--- Student Records ---")
+
+    with open(student_database, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for x in student_fields:
+            print(x, end='\t |')
+        print("\n  ")
+
+        for row in reader:
+            for item in row:
+                print(item, end="\t |")
+            print("\n")
+
+    input("Press any key to continue")
+
+def update_student():
+    global student_fields
+    global student_database
+
+    print(" Update Student ")
+    roll = input("Enter roll no. to update: ")
+    index_student = None
+    updated_data = []
+    with open(student_database, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        counter = 0
+        for row in reader:
+            if len(row) > 0:
+                if roll == row[0]:
+                    index_student = counter
+                    print("Student Found: at index ", index_student)
+                    student_data = []
+                    for field in student_fields:
+                        value = input("Enter " + field + ": ")
+                        student_data.append(value)
+                    updated_data.append(student_data)
+                else:
+                    updated_data.append(row)
+                counter += 1
+
+    if index_student is not None:
+        with open(student_database, "w", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerows(updated_data)
+    else:
+        print("Roll No. not found in our database")
+
+    input("Press any key to continue")
+
+
+def delete_student():
+    global student_fields
+    global student_database
+
+    print(" Delete Student ")
+    roll = input("Enter roll no. to delete: ")
+    student_found = False
+    updated_data = []
+    with open(student_database, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        counter = 0
+        for row in reader:
+            if len(row) > 0:
+                if roll != row[0]:
+                    updated_data.append(row)
+                    counter += 1
+                else:
+                    student_found = True
+
+    if student_found is True:
+        with open(student_database, "w", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerows(updated_data)
+        print("Roll no. ", roll, "deleted successfully")
+    else:
+        print("Roll No. not found in our database")
+
+    input("Press any key to continue")
+
+
+while True:
+    display_menu()
+
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        add_student()
+    elif choice == '2':
+        view_students()
+    elif choice == '3':
+        update_student()
+    elif choice == '4':
+        delete_student()
+    else:
+        break
